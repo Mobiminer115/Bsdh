8b47768d433cfb22c0c29ac39a2d6c97caee927d
Mobiminer115 <111297923+Mobiminer115@users.noreply.github.com>
2026-07-10 10:09:34 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,15 @@ let package = Package(
             exclude: ["Metal/UpscaleShaders.metal", "Example"],
             publicHeadersPath: "Public",
             cSettings: [.unsafeFlags(["-fobjc-arc"])],
-            cxxSettings: [.unsafeFlags(["-fobjc-arc"])]
+            cxxSettings: [.unsafeFlags(["-fobjc-arc"])],
+            linkerSettings: [
+                .linkedFramework("UIKit"),
+                .linkedFramework("Foundation"),
+                .linkedFramework("QuartzCore"),
+                .linkedFramework("CoreGraphics"),
+                .linkedFramework("Metal"),
+                .linkedLibrary("objc")
+            ]
         )
     ],
     cxxLanguageStandard: .gnucxx17
